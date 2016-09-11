efffb1b6d625a9563fca6df6bfa17f980f3c514a
Sean-PC\Sean <seanreid.mail@gmail.com>
2016-09-11 00:50:42 -0700

    Implemented linear operator functions in place of matrix operations, to use scipy sparse library optimizer. Also implemented DCT

--- code/main.py
+++ code/main.py
@@ -2,7 +2,7 @@ import numpy as np
 from matplotlib import pyplot as plt
 from scipy import sparse
 from scipy.fftpack import dct
-from sklearn import linear_model
+from scipy.sparse import linalg
 from scipy.ndimage.interpolation import zoom
 
 # All based on work from http://www.scielo.org.co/pdf/iei/v34n3/v34n3a09.pdf
@@ -71,6 +71,29 @@ def genDecimationMatrix(N,M,L,Delta):
 	D = sparse.csr_matrix((data,(r,c)),shape=(N*(M+L-1)/Delta**2,N*(M+L-1)))
 	return D
 
+def dct3(x,N,M,L):
+	x = np.reshape(x,(N,M,L))
+	X = dct(dct(dct(x).transpose(0,2,1)).transpose(1,2,0)).transpose(1,2,0).transpose(0,2,1)
+	X = np.reshape(X,(N*M*L,))
+	return X
+
+def idct3(X,N,M,L):
+	X = np.reshape(X,(N,M,L))
+	x = dct(dct(dct(X).transpose(1,2,0)).transpose(0,2,1)).transpose(0,2,1).transpose(1,2,0)
+	x = np.reshape(x,(N*M*L,))
+	return x
+
+def A_forward(theta,X,N,M,L):
+	f = idct3(theta,N,M,L)
+	g = X.dot(f)
+	return g
+
+def A_backward(g,X,N,M,L):
+	Xt = X.T
+	f = Xt.dot(g)
+	theta = dct3(f,N,M,L)
+	return theta
+
 data = readData(impath,N0,M0,L0,zfactor)
 
 T1 = genMask1Matrix(N,M,L)
@@ -82,13 +105,13 @@ H = T2.dot(P.dot(T1))
 f_img = np.reshape(data,(N*M*L,1))
 
 X = D.dot(H)
-y = X.dot(f_img)
+A = linalg.LinearOperator(X.shape,matvec = lambda theta: A_forward(theta,X,N,M,L),rmatvec = lambda g: A_backward(g,X,N,M,L))
+g = A.dot(f_img)
 f0 = np.zeros((N*M*L,))
 
-clf = linear_model.Lasso(alpha = 0.1)
-clf.fit(X,y)
+theta = linalg.lsqr(A,g)
 
-f_v = clf.coef_
+f_v = idct3(theta,N,M,L)
 f = np.reshape(f_v,(N,M,L))
 
 img = np.log(f.mean(axis=2))
