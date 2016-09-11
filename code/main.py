3cce8ec6351104b6d87eee1aaddd4cc77d579e9b
Sean-PC\Sean <seanreid.mail@gmail.com>
2016-09-11 02:19:41 -0700

    Minor changes, debugging linear operator- something is wrong with decimation matrix

--- code/main.py
+++ code/main.py
@@ -11,13 +11,14 @@ N0 = 1160
 M0 = 320
 L0 = 360
 
-zfactor = 0.125
+zfactor = 0.25
+offset = 1
 
 N = int(N0*zfactor)
 M = int(M0*zfactor)
-L = int(L0*zfactor - 1)
+L = int(L0*zfactor - offset)
 
-Delta = 4
+Delta = 2
 
 np.random.seed(0)
 
@@ -30,7 +31,7 @@ def readData(fname,N0,M0,L0,zfactor):
 	data = np.reshape(data,(N0,L0,M0))
 	data = np.swapaxes(data,1,2)
 	data = zoom(data,zfactor)
-	return data[:,:,:-1]
+	return data[:,:,:-offset]
 
 def genMask1Matrix(N,M,L):
 	m_v = np.round(np.random.rand(N*M,))
@@ -40,8 +41,15 @@ def genMask1Matrix(N,M,L):
 	T = sparse.csr_matrix((data,(r,c)),shape=(N*M*L,N*M*L))
 	return T
 
+# def genMask2Matrix(N,M,L):
+# 	data = np.round(np.random.rand(N*(M+L-1),))
+# 	r = np.arange(N*(M+L-1))
+# 	c = np.arange(N*(M+L-1))
+# 	T = sparse.csr_matrix((data,(r,c)),shape=(N*(M+L-1),N*(M+L-1)))
+# 	return T
+
 def genMask2Matrix(N,M,L):
-	data = np.round(np.random.rand(N*(M+L-1),))
+	data = np.ones((N*(M+L-1),))
 	r = np.arange(N*(M+L-1))
 	c = np.arange(N*(M+L-1))
 	T = sparse.csr_matrix((data,(r,c)),shape=(N*(M+L-1),N*(M+L-1)))
@@ -67,7 +75,7 @@ def genDecimationMatrix(N,M,L,Delta):
 	for i in range(int((M+L-1)/Delta)):
 		for j in range(int(N/Delta)):
 			r[i*N*Delta + j*Delta**2:i*N*Delta + j*Delta**2 + Delta**2] = r0 + i*N/Delta + j
-			c[i*N*Delta + j*Delta**2:i*N*Delta + j*Delta**2 + Delta**2] = np.roll(c0,j*Delta) + i*Delta**2
+			c[i*N*Delta + j*Delta**2:i*N*Delta + j*Delta**2 + Delta**2] = c0 + j*Delta + i*Delta**2
 	D = sparse.csr_matrix((data,(r,c)),shape=(N*(M+L-1)/Delta**2,N*(M+L-1)))
 	return D
 
@@ -99,22 +107,29 @@ data = readData(impath,N0,M0,L0,zfactor)
 T1 = genMask1Matrix(N,M,L)
 T2 = genMask2Matrix(N,M,L)
 P = genDispMatrix(N,M,L)
-D = genDecimationMatrix(N,M,L,Delta)
+# D = genDecimationMatrix(N,M,L,Delta)
 H = T2.dot(P.dot(T1))
 
 f_img = np.reshape(data,(N*M*L,1))
 
-X = D.dot(H)
+X = H
 A = linalg.LinearOperator(X.shape,matvec = lambda theta: A_forward(theta,X,N,M,L),rmatvec = lambda g: A_backward(g,X,N,M,L))
-g = A.dot(f_img)
-f0 = np.zeros((N*M*L,))
+theta0 = dct3(f_img,N,M,L)
+g = A.dot(theta0)
 
-theta = linalg.lsqr(A,g)
+# im = np.reshape(g,(int(N/Delta),int((M+L-1)/Delta)))
+im = np.reshape(g,(N,(M+L-1)))
+
+result = linalg.lsqr(A,g,damp=0.1,show=1,iter_lim=500)
+
+theta = result[0]
 
 f_v = idct3(theta,N,M,L)
 f = np.reshape(f_v,(N,M,L))
 
-img = np.log(f.mean(axis=2))
+img = im.mean(axis=2)
 
 plt.imshow(img,cmap='Greys_r')
+plt.show()
+plt.imshow(data.mean(axis=2),cmap='Greys_r')
 plt.show()
