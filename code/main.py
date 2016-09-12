901bfd79b92c9427e7e3e5694a4914e8a182f420
Sean-PC\Sean <seanreid.mail@gmail.com>
2016-09-11 19:48:26 -0700

    Implementation of spgl_lasso optimizer

--- code/main.py
+++ code/main.py
@@ -4,8 +4,11 @@ from scipy import sparse
 from scipy.fftpack import dct
 from scipy.fftpack import idct
 from scipy.sparse import linalg
+from scipy.misc import imresize
 from sklearn.linear_model import Lasso
 from NESTA import NESTA
+from spgl1 import *
+from spgl_aux import * 
 from scipy.ndimage.interpolation import zoom
 
 # All based on work from http://www.scielo.org.co/pdf/iei/v34n3/v34n3a09.pdf
@@ -69,6 +72,20 @@ def genDispMatrix(N,M,L):
 	P = sparse.csr_matrix((data,(r,c)),shape=(N*(M+L-1),N*M*L))
 	return P
 
+# def genDispMatrix(N,M,L):
+# 	data = np.ones((N*M*L,))
+# 	c0 = np.arange(0,N*M,M)
+# 	r0 = np.arange(N*M)
+# 	c = np.empty(N*M*L)
+# 	r = np.empty(N*M*L)
+# 	for i in range(L):
+# 		for j in range(M):
+# 			c[i*N : (i+1)*N] = c0+i*N*M+j
+# 			print(c0+i*N*M+j)
+# 		r[i*N*M : (i+1)*N*M] = r0+i*N
+# 	P = sparse.csr_matrix((data,(r,c)),shape=(N*(M+L-1),N*M*L))
+# 	return P
+
 def genDecimationMatrix(N,M,L,Delta):
 	data0 = np.ones((Delta**2,))
 	r0 = np.zeros((Delta**2,))
@@ -95,77 +112,124 @@ def idct3(X,N,M,L):
 	x = np.reshape(x,(N*M*L,))
 	return x
 
-def A_forward(theta,X,N,M,L):
+def A_forward(theta,X,N,M,L,Delta):
 	f = idct3(theta,N,M,L)
 	g = X.dot(f)
-	return g
+	g_im = g.reshape((M+L-1,N))
+	# plt.imshow(g_im,cmap='Greys_r',interpolation='none')
+	# plt.show()
+	g_d = imresize(g_im,1./Delta)
+	g_v = g_d.reshape((int(N*(M+L-1)/Delta**2),))
+	return g_v
 
-def A_backward(g,X,N,M,L):
+def A_backward(g,X,N,M,L,Delta):
+	g_d = g.reshape((int((M+L-1)/Delta),int(N/Delta)))
+	g_im = zoom(g_d,Delta,order=0)
+	g = g_im.reshape((N*(M+L-1),))
 	Xt = X.T
 	f = Xt.dot(g)
 	theta = dct3(f,N,M,L)
 	return theta
 
+def Amatrix(x,mode,Af,Ab):
+	if mode == 1:
+		return Af(x)
+	else:
+		return Ab(x)
 
-# data = readData(impath,N0,M0,L0,zfactor,offset,Nmax)
+data = readData(impath,N0,M0,L0,zfactor,offset,Nmax)
 
-N = 16
-M = 16
-L = 7
-Delta = 2
+# N = 20
+# M = 20
+# L = 15
+# Delta = 2
 
-l1 = np.ones((N,M))
-
-data = np.stack((l1,2*l1,3*l1,l1,0.5*l1,l1,0.5*l1),axis=2)
+# # l1 = np.ones((N,M))
+# # data = np.zeros((N,M,L))
+# data = np.ones((N,M,L))
+# data[3:7,3:8,:] = np.zeros((4,5,15))
+# im_data = np.random.rand(N,M+L-1)
+# im_v =im_data.reshape((N*(M+L-1),))
+# data = np.stack((l1,2*l1,3*l1,l1,0.5*l1,l1,0.5*l1),axis=2)
 
 T1 = genMask1Matrix(N,M,L)
 T2 = genMask2Matrix(N,M,L)
 P = genDispMatrix(N,M,L)
-D = genDecimationMatrix(N,M,L,Delta)
+# D = genDecimationMatrix(N,M,L,Delta)
 H = T2.dot(P.dot(T1))
 
+data = data.transpose(2,1,0)
 f_img = np.reshape(data,(N*M*L,))
 
-X = D.dot(H)
-X = P.dot(T1)
-# X = H
-# X = P.dot(T1)
-Af = lambda theta: A_forward(theta,X,N,M,L)
-Ab = lambda g: A_backward(g,X,N,M,L)
-A = X
+# print(f_img)
+
+# im_Dv = D.dot(im_v)
+# im_D = np.reshape(im_Dv,(int(N/Delta),int((M+L-1)/Delta)))
+# im_Ds = im_D#.swapaxes(0,1)
+
+X = T2.dot(P.dot(T1))
+# X = P
+Af = lambda theta: A_forward(theta,X,N,M,L,Delta)
+Ab = lambda g: A_backward(g,X,N,M,L,Delta)
+# A = X
 # A = linalg.LinearOperator(X.shape,matvec = Af,rmatvec = Ab)
+A = lambda x,mode: Amatrix(x,mode,Af,Ab)
 theta0 = dct3(f_img,N,M,L)
-g = Af(theta0)
+# print(np.mean(theta0))
+# g = A.dot(theta0)
+g = A(theta0,1)
 
 # print((int(N/Delta),int((M+L-1)/Delta)))
 # print(g.size)
 
-# im = np.reshape(g,(int(N/Delta),int((M+L-1)/Delta)))
-im = np.reshape(g,((M+L-1),N))
+im = np.reshape(g,(int((M+L-1)/Delta),int(N/Delta)))
 im = im.swapaxes(0,1)
+# im = np.reshape(g,((M+L-1), N))
 
 # U = lambda x: dct3(x,N,M,L)
 # Ut = lambda X: idct3(X,N,M,L)
 
-opts = {'U':sparse.identity(N*M*L),'normU':1}
+# opts = {'U':sparse.identity(N*M*L),'normU':1}
 
-muf = 1e-8
-sigma = 0.001
-delta = np.sqrt(g.size + 2*np.sqrt(2*g.size))*sigma
-delta = 1e-8
-theta,niter,residuals,outputData = NESTA(A=Af,At=Ab,b=g,muf=muf,delta=delta,opts = opts)
+# muf = 1e-8
+# sigma = 0.001
+# delta = np.sqrt(g.size + 2*np.sqrt(2*g.size))*sigma
+# delta = 1e-8
+# theta,niter,residuals,outputData = NESTA(A=Af,At=Ab,b=g,muf=muf,delta=delta,opts = opts)
 
-# result = linalg.lsqr(A,g,damp=0.1,show=1,iter_lim=50)
+# result = linalg.lsmr(A,g,damp=0.1,show=1)
+
+tau = 0
+theta,resid,grad,info = spg_lasso(A, g, tau)
 
 # theta = result[0]
 
 f_v = idct3(theta,N,M,L)
-f = np.reshape(f_v,(N,M,L))
+f = np.reshape(f_v,(L,N,M))  
+f = f.transpose(1,2,0) 
+data = data.transpose(2,1,0)
 
 # img = im.mean(axis=2)
 
-plt.imshow(f.mean(axis=2),cmap='Greys_r')
+# print(f.mean(axis=2))
+
+f1 = plt.figure()
+f2 = plt.figure()
+f3 = plt.figure()
+
+ax1 = f1.add_subplot(111)
+ax2 = f2.add_subplot(111)
+ax3 = f3.add_subplot(111)
+
+ax1.imshow(data.mean(axis=2),cmap='Greys_r',interpolation='none')
+ax2.imshow(f.mean(axis=2),cmap='Greys_r',interpolation='none')
+ax3.imshow(im,cmap='Greys_r',interpolation='none')
+
 plt.show()
 
-plt.imshow(im,cmap='Greys_r')
-plt.show()
+# print(np.sum((f - data)**2))
+# print(f)
+
+print(P.dot(f_img).reshape((M+L-1,N)).swapaxes(0,1))
+print(f)
+print(np.sum(theta0<0.1))
